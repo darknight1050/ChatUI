0421d8570fce9e284a8c579746c9fe2e43704284
darknight1050 <darkdeveloping1050@gmail.com>
2021-02-21 14:58:28 +0100

    Add underscore button

--- src/ModSettingsViewController.cpp
+++ src/ModSettingsViewController.cpp
@@ -17,17 +17,33 @@ void DidActivate(ViewController* self, bool firstActivation, bool addedToHierarc
         self->get_gameObject()->AddComponent<Touchable*>();
 
         GameObject* container = BeatSaberUI::CreateScrollableSettingsContainer(self->get_transform());
+        Transform* parent = container->get_transform();
 
-        AddConfigValueStringSetting(container->get_transform(), getModConfig().Channel);
+        auto layout = BeatSaberUI::CreateHorizontalLayoutGroup(parent);
+        layout->GetComponent<LayoutElement*>()->set_preferredWidth(90.0f);
+        layout->set_childControlWidth(true);
+        auto layoutParent = layout->get_transform();
+        
+        auto stringSetting = AddConfigValueStringSetting(layoutParent, getModConfig().Channel);
 
-        AddConfigValueIncrementVector3(container->get_transform(), getModConfig().PositionMenu, 2, 0.05f);
-        AddConfigValueIncrementVector3(container->get_transform(), getModConfig().RotationMenu, 0, 1.0f);
-        AddConfigValueIncrementVector2(container->get_transform(), getModConfig().SizeMenu, 0, 1.0f);
+        //Use underline from button because text doesn't work somehow with this width
+        auto underscoreButton = BeatSaberUI::CreateUIButton(layoutParent, "", UnityEngine::Vector2(0.0f, 0.0f), UnityEngine::Vector2(4.0f, 8.0f), [stringSetting] {
+            stringSetting->KeyboardKeyPressed('_');
+            stringSetting->UpdateClearButton();
+        });
+        BeatSaberUI::AddHoverHint(underscoreButton->get_gameObject(), "Adds a underscore");
+        //Hack fix for questui bug
+        LayoutElement* layoutElement = underscoreButton->GetComponent<LayoutElement*>();
+        layoutElement->set_minWidth(4.0f);
 
-        AddConfigValueToggle(container->get_transform(), getModConfig().ForceGame);
+        AddConfigValueIncrementVector3(parent, getModConfig().PositionMenu, 2, 0.05f);
+        AddConfigValueIncrementVector3(parent, getModConfig().RotationMenu, 0, 1.0f);
+        AddConfigValueIncrementVector2(parent, getModConfig().SizeMenu, 0, 1.0f);
 
-        AddConfigValueIncrementVector3(container->get_transform(), getModConfig().PositionGame, 2, 0.05f);
-        AddConfigValueIncrementVector3(container->get_transform(), getModConfig().RotationGame, 0, 1.0f);
-        AddConfigValueIncrementVector2(container->get_transform(), getModConfig().SizeGame, 0, 1.0f);
+        AddConfigValueToggle(parent, getModConfig().ForceGame);
+
+        AddConfigValueIncrementVector3(parent, getModConfig().PositionGame, 2, 0.05f);
+        AddConfigValueIncrementVector3(parent, getModConfig().RotationGame, 0, 1.0f);
+        AddConfigValueIncrementVector2(parent, getModConfig().SizeGame, 0, 1.0f);
     }
 }
